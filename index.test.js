8f337d5fdcddad8f9d184221e2bec9d0b3edf624
glebbash <glebbash@gmail.com>
2022-03-18 15:49:40 +0000

    chore: try with catch

--- index.test.js
+++ index.test.js
@@ -1,71 +1,57 @@
 const assert = require("node:assert");
 const { mkdir, readFile, rm, lstat } = require("node:fs/promises");
 const { chdir } = require("node:process");
-
 const exec = require("node:util").promisify(require("node:child_process").exec);
 
 const IMAGE_NAME = "extenda/unwrap-template";
 
-testTemplateUnwrapping();
+testSuite(
+  async function testTemplateUnwrapping() {
+    try {
+      await exec(`docker build . -t ${IMAGE_NAME}`);
 
-async function testTemplateUnwrapping() {
-  try {
-    await exec(`docker build . -t ${IMAGE_NAME}`);
+      chdir("test");
+      await exec("bash .template/init.sh");
 
-    chdir("test");
-    await exec("bash .template/init.sh");
+      const content = await readFile("hello.txt", { encoding: "utf-8" });
 
-    const content = await readFile("hello.txt", { encoding: "utf-8" });
+      assert.equal(content, "Hello World!\n");
+    } finally {
+      await rm("hello.txt");
+      chdir("..");
+    }
+  },
 
-    assert.equal(content, "Hello World!\n");
-  } finally {
-    await rm("hello.txt");
-    chdir("..");
+  async function testTemplateRepoInit() {
+    try {
+      await exec(`docker build . -t ${IMAGE_NAME}`);
+
+      await mkdir("test/meta-test");
+      chdir("test/meta-test");
+
+      await exec(`docker run -v $PWD:/data -e INIT=true ${IMAGE_NAME}`);
+
+      assert.ok((await lstat(".template/files/README.md")).isFile());
+      assert.ok((await lstat(".template/options.json")).isFile());
+      assert.ok((await lstat(".template/README.md")).isFile());
+      assert.ok((await lstat(".template/init.sh")).isFile());
+      assert.ok((await lstat("README.md")).isSymbolicLink());
+    } finally {
+      chdir("../..");
+      await rm("test/meta-test", { recursive: true });
+    }
+  }
+);
+
+async function testSuite(...tests) {
+  for (const test of tests) {
+    process.stdin.write(`${test.name}: `);
+    try {
+      await test();
+      process.stdin.write(`OK\n`);
+    } catch (error) {
+      process.stdin.write(`FAIL\n`);
+      throw error;
+    }
   }
 }
-
-// testSuite(
-//   async function testTemplateUnwrapping() {
-//     try {
-//       await exec(`docker build . -t ${IMAGE_NAME}`);
-
-//       chdir("test");
-//       await exec("bash .template/init.sh");
-
-//       const content = await readFile("hello.txt", { encoding: "utf-8" });
-
-//       assert.equal(content, "Hello World!\n");
-//     } finally {
-//       await rm("hello.txt");
-//       chdir("..");
-//     }
-//   },
-
-//   async function testTemplateRepoInit() {
-//     try {
-//       await exec(`docker build . -t ${IMAGE_NAME}`);
-
-//       await mkdir("test/meta-test");
-//       chdir("test/meta-test");
-
-//       await exec(`docker run -v $PWD:/data -e INIT=true ${IMAGE_NAME}`);
-
-//       assert.ok((await lstat(".template/files/README.md")).isFile());
-//       assert.ok((await lstat(".template/options.json")).isFile());
-//       assert.ok((await lstat(".template/README.md")).isFile());
-//       assert.ok((await lstat(".template/init.sh")).isFile());
-//       assert.ok((await lstat("README.md")).isSymbolicLink());
-//     } finally {
-//       chdir("../..");
-//       await rm("test/meta-test", { recursive: true });
-//     }
-//   }
-// );
-
-// async function testSuite(...tests) {
-//   for (const test of tests) {
-//     process.stdin.write(`${test.name}: `);
-//     await test();
-//     process.stdin.write(`OK\n`);
-//   }
-// }
